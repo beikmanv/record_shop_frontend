88c7277e00363ac728c5b03f8b0d0452f726a551
Valdis <v.beikmanis@gmail.com>
2025-01-07 14:20:32 +0300

    feat: drawable icons added, activity_main.xml updated

--- app/src/main/java/com/northcoders/mvvmhttprequestswithretrofit/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/mvvmhttprequestswithretrofit/ui/mainactivity/MainActivity.java
@@ -2,7 +2,10 @@ package com.northcoders.mvvmhttprequestswithretrofit.ui.mainactivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
@@ -10,6 +13,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationBarView;
 import com.northcoders.mvvmhttprequestswithretrofit.R;
 import com.northcoders.mvvmhttprequestswithretrofit.adapter.AlbumAdapter;
 import com.northcoders.mvvmhttprequestswithretrofit.databinding.ActivityMainBinding;
@@ -20,54 +24,62 @@ import com.northcoders.mvvmhttprequestswithretrofit.ui.updatealbum.UpdateAlbumAc
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements RecyclerViewInterface {
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
-    private RecyclerView recyclerView;
-    private ArrayList<Album> albumList = new ArrayList<>();
-    private AlbumAdapter albumAdapter;
-    private MainActivityViewModel viewModel;
-    private ActivityMainBinding binding;
-    private AlbumRepository albumRepository;
-    private static final String ALBUM_KEY = "album";
-    private MainActivityClickHandler handler;
+//    private RecyclerView recyclerView;
+//    private ArrayList<Album> albumList = new ArrayList<>();
+//    private AlbumAdapter albumAdapter;
+//    private MainActivityViewModel viewModel;
+//    private ActivityMainBinding binding;
+//    private AlbumRepository albumRepository;
+//    private static final String ALBUM_KEY = "album";
+//    private MainActivityClickHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
-        handler = new MainActivityClickHandler(this);
-        binding.setClickHandler(handler);
-        getAllAlbums();
-    }
+        setContentView(R.layout.activity_main);
 
-    private void getAllAlbums() {
-        viewModel.getAlbums().observe(this, new Observer<List<Album>>() {
-            @Override
-            public void onChanged(List<Album> albumsFromLiveData) {
-                        albumList = (ArrayList<Album>) albumsFromLiveData;
-                    displayInRecyclerView();
-                }
-        });
-    }
 
-    private void displayInRecyclerView() {
-        recyclerView = binding.recyclerView;
-        // Pass "this" as the RecyclerViewInterface to handle clicks
-        albumAdapter = new AlbumAdapter(albumList, this);
-        recyclerView.setAdapter(albumAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-        albumAdapter.notifyDataSetChanged();
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+//        handler = new MainActivityClickHandler(this);
+//        binding.setClickHandler(handler);
+//        getAllAlbums();
     }
 
     @Override
-    public void onItemClick(int position) {
-        // Create an Intent to navigate to UpdateAlbumActivity
-        Intent intent = new Intent(MainActivity.this, UpdateAlbumActivity.class);
-        // Pass the selected Album object as a Parcelable
-        intent.putExtra(ALBUM_KEY , albumList.get(position));
-        // Start the UpdateAlbumActivity
-        startActivity(intent);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
+
+//    private void getAllAlbums() {
+//        viewModel.getAlbums().observe(this, new Observer<List<Album>>() {
+//            @Override
+//            public void onChanged(List<Album> albumsFromLiveData) {
+//                        albumList = (ArrayList<Album>) albumsFromLiveData;
+//                    displayInRecyclerView();
+//                }
+//        });
+//    }
+
+//    private void displayInRecyclerView() {
+//        recyclerView = binding.recyclerView;
+//        // Pass "this" as the RecyclerViewInterface to handle clicks
+//        albumAdapter = new AlbumAdapter(albumList, this);
+//        recyclerView.setAdapter(albumAdapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setHasFixedSize(true);
+//        albumAdapter.notifyDataSetChanged();
+//    }
+//
+//    @Override
+//    public void onItemClick(int position) {
+//        // Create an Intent to navigate to UpdateAlbumActivity
+//        Intent intent = new Intent(MainActivity.this, UpdateAlbumActivity.class);
+//        // Pass the selected Album object as a Parcelable
+//        intent.putExtra(ALBUM_KEY , albumList.get(position));
+//        // Start the UpdateAlbumActivity
+//        startActivity(intent);
+//    }
 }
